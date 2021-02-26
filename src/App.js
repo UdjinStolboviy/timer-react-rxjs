16453b309b52f6aecb6c9046874bdea31a32acb4
Your Name <JenyJenyy@gmail.com>
2021-02-26 17:36:46 +0200

    Added styles and structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import "./App.css";
 
-function App() {
+function Timer() {
   return (
     <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span> 12:13:14</span>
+      <button className="button-start">Start</button>
+      <button className="button-stop">Stop</button>
+      <button className="button-reset">Reset</button>
+      <button className="button-wait">Wait</button>
     </div>
   );
 }
 
-export default App;
+export default Timer;
